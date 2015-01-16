572abbe5a112ae7493ab4341aca6522c082f4cbd
unknown <male@Mathieu-L-205.MenLeul.local>
2015-01-16 12:04:34 +0100

    Last commit Leul

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php setcookie('pseudo', 'test_cook', time() + 365*24*3600, null, null, false, true); ?>
 
 <?php
+$monfichier = fopen('compteur.txt', 'r+');
+ 
+$pages_vues = fgets($monfichier); // On lit la première ligne (nombre de pages vues)
+$pages_vues++; // On augmente de 1 ce nombre de pages vues
+fseek($monfichier, 0); // On remet le curseur au début du fichier
+fputs($monfichier, $pages_vues); // On écrit le nouveau nombre de pages vues
+ 
+fclose($monfichier);
+ 
+echo '<p>Cette page a été vue ' . $pages_vues . ' fois !</p>';
+?>
+
+<?php
 // Le mot de passe n'a pas été envoyé ou n'est pas bon
 if (!isset($_POST['mot_de_passe']) OR $_POST['mot_de_passe'] != "kangourou")
 {
